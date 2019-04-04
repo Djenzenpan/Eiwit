791481ebf67a1db2209bfd57cf9fe62e4d19b065
Djenzenpan <jmppannekeet@gmail.com>
2019-04-04 17:12:43 +0200

    Score berekenen

--- protein.py
+++ protein.py
@@ -4,3 +4,66 @@ class Protein(object):
         self.length = len(protein)
         self.dimension = (self.length * 2) - 1
         self.field = [["_"] * self.dimension for i in range(self.dimension)]
+        self.current_option = ["right" for i in range(self.length - 2)]
+        self.field[self.length - 1][self.length - 1] = self.sequence[0]
+        self.field[self.length][self.length - 1] = self.sequence[1]
+        self.number = 0
+        self.best_fold_points = 0
+        self.best_fold = None
+
+    """ creates a field based on the current_option"""
+    def create_field(self, x, y):
+        for aminoacid, option in zip(self.sequence[2:], range(len(self.current_option))):
+           if self.current_option[option] == "right":
+               if self.field[y][x+1] == "_":
+                   self.field[y][x+1] = aminoacid
+                   x = x + 1
+               else:
+                   self.current_option[option] = "up"
+           if self.current_option[option] == "up":
+               if self.field[y-1][x+1] == "_":
+                   self.field[y-1][x] = aminoacid
+                   y = y - 1
+               else:
+                   self.current_option[option] = "left"
+           if self.current_option[option] == "left":
+               if self.field[y][x-1] == "_":
+                   self.field[y][x-1] = aminoacid
+                   x = x - 1
+               else:
+                   self.current_option[option] = "down"
+           if self.current_option[option] == "down":
+               if self.field[y+1][x] == "_":
+                   self.field[y+1][x] = aminoacid
+                   y = y + 1
+               else:
+                   self.current_option[option] = "right"
+
+    """ calculates current_option fields total points"""
+    def fold_points(self):
+        points = 0
+        for i in range(self.dimension):
+           for j in range(self.dimension):
+               if self.field[j][i] == "H":
+                   if self.field[j+1][i] == "H":
+                       points += 1
+                   if self.field[j-1][i] == "H":
+                       points += 1
+                   if self.field[j][i-1] == "H":
+                       points += 1
+                   if self.field[j][i+1] == "H":
+                       points += 1
+        for i in range(self.length - 1):
+                if self.sequence[i] == self.sequence[i + 1] == "H":
+                    points -= 1
+        for i in range(self.length):
+            if i != 0 and self.sequence[i] == self.sequence[i - 1] == "H":
+                    points -= 1
+        return(int(points / 2))
+
+    """checks whether current option is the best option yet"""
+    def check_option(self):
+        self.create_field((self.length - 1),self.length)
+        if self.fold_points() > self.best_fold_points:
+            self.best_fold_points = self.fold_points()
+            self.best_fold = self.current_option
--- versie0.py
+++ versie0.py
@@ -6,28 +6,19 @@ def main():
     check()
     # makes user input into the protein class
     protein = Protein(argv[1])
-    # initialises first aminoacid directly above the middle of the field and
-    # the second aminoacid in the middle of the field
-    protein.field[protein.length - 1][protein.length - 1] = protein.sequence[0]
-    protein.field[protein.length][protein.length - 1] = protein.sequence[1]
-    #
-    # TODO
-    # protein.check_best
-    #
-    # remembers x and y coordinates of current aminoacid
-    x = protein.length - 1
-    y = protein.length
-
-    ### configures protein to go down in a straight line
-#    for aminoacid in protein.sequence[2:]:
-#        if protein.field[y+1][x] == "_":
-#            protein.field[y+1][x] = aminoacid
-#            y = y+1
-#        elif protein.field[y][x+1] == "_":
-#            protein.field[y][x+1] = aminoacid
-#            x = x+1
-
-    # prints field
+    # folds protein for testing
+    if len(protein.current_option) > 7:
+        protein.current_option[1] = "up"
+        protein.current_option[2] = "up"
+        protein.current_option[3] = "left"
+        protein.current_option[4] = "left"
+        protein.current_option[5] = "down"
+        protein.current_option[6] = "down"
+    # checks whether current option is better than all previous ones
+    protein.check_option()
+    # prints best_fold_points and best_fold and best field
+    print(protein.best_fold_points)
+    print(protein.best_fold)
     for line in protein.field:
         print(line)
 
